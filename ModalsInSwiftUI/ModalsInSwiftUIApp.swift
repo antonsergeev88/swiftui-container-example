f060c7841d869b44a52c66a57d614b7847d0ebd7
Anton Sergeev <antonsergeev88@gmail.com>
2023-01-13 13:05:07 +0400

    Initial Commit

---
+++ ModalsInSwiftUI/ModalsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalsInSwiftUIApp.swift
+//  ModalsInSwiftUI
+//
+//  Created by Anton Sergeyev on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ModalsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
